e74ab0330f8cb5fc0460c468e4831024eac2de78
StepanPC <stepan.karpov@list.ru>
2023-04-21 01:05:00 +0300

    interface updated

--- interface/trajectory_calculation_constants.py
+++ interface/trajectory_calculation_constants.py
@@ -1,9 +1,17 @@
 INIT_FLIGH_DATA = {
   "height": -1,
-  "ttw": -1
+  "ttw": -1,
+  "velocity": -1,
+  "thrust": -1,
+  "g": -1,
+  "pressure": -1
 }
 
 INIT_VESSEL_DATA = {
-  "height": -1,
-  "ttw": -1
+  "fuel on current stage": -1,
+  "mass of the vessel": -1,
+  "delta v": -1,
+  "electricity": -1,
+  "monofuel": -1,
+  "fuel ejection ": -1,
 }
--- interface/window.py
+++ interface/window.py
@@ -1,46 +1,61 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor, QFont
+from PyQt5.QtCore import Qt, QTimer
 import pyqtgraph
 from interface.interface_constants import *
 from interface.trajectory_calculation_constants import *
+from flight_info.info import Info
+from trajectory_calculation.trajectory_predictor import Trajectory
 
 
 class Plot():
   def __init__(self, title, y_name, x_name):
     self.graph_widget = pyqtgraph.PlotWidget()
 
-    hour = [1,2,3,4,5,6,7,8,9,10]
-    temperature = [30,32,34,32,33,31,29,32,35,45]
-
     self.graph_widget.setTitle(title, color="black", size="10pt")
 
     styles = {'color':'black', 'font-size':'15px'}
     self.graph_widget.setLabel('left', y_name, **styles)
     self.graph_widget.setLabel('bottom', x_name, **styles)
-    self.graph_widget.setBackground('white')
+    self.graph_widget.setBackground('grey')
     self.graph_widget.setBackground('#bbccaa')
 
-    pen = pyqtgraph.mkPen(color=(0, 0, 0), width=5)
-    self.graph_widget.plot(hour, temperature, pen=pen)
-
     self.graph_widget.showGrid(x=True, y=True)
-    self.graph_widget.plot(hour, temperature)
 
+
+
+    self.predict_trajectory_y, self.predict_trajectory_x = Trajectory.get_r_y_r_x()
+    pen = pyqtgraph.mkPen(color=(0, 0, 0), width=3)
+    self.graph_widget.plot(self.predict_trajectory_y, self.predict_trajectory_x, pen=pen)
+
+    self.real_trajectory_y = []
+    self.real_trajectory_x = []
+
+      
   def get_widget(self):
     return self.graph_widget
+
+  def add_predict_point(self, point):
+    pen = pyqtgraph.mkPen(color=(0, 0, 0), width=3)
+
+    self.real_trajectory_y.append(point[0])
+    self.real_trajectory_x.append(point[1])
+
+    # self.graph_widget.clear()
+    self.graph_widget.plot(self.real_trajectory_y, self.real_trajectory_x, pen=pen)
+
 
 class InfoWidget():
   def __init__(self, flight_data, vessel_data):
     text_layout = QHBoxLayout()
-    text_layout.addWidget(self.get_info_widget('grey', flight_data))
-    text_layout.addWidget(self.get_info_widget('grey', vessel_data))
+    text_layout.addWidget(self.get_info_widget('grey', flight_data, "fligh data"))
+    text_layout.addWidget(self.get_info_widget('grey', vessel_data, "vessel data"))
     self.control_widget = QWidget()
     self.control_widget.setLayout(text_layout)
 
 
-  def get_info_widget(self, color, data: dict):
+  def get_info_widget(self, color, data: dict, header):
     self.info_widget = QWidget()
     self.info_widget.setAutoFillBackground(True)
 
@@ -49,9 +64,20 @@ class InfoWidget():
     self.info_widget.setPalette(palette)
 
     layout = QVBoxLayout()
+    
+    header_box = QHBoxLayout()
+
+    header_label = QLabel("")
+    header_label2 = QLabel(header)
+    header_label2.setFont(QFont("Sanserif", 12))
+    header_box.addWidget(header_label, stretch=1)
+    header_box.addWidget(header_label2, stretch=2)
+
+    layout.addLayout(header_box)
 
     for name, value in data.items():
       object = QLabel(str(name) + ": " + str(value))
+      object.setFont(QFont("Sanserif", 10))
       layout.addWidget(object)
 
     self.info_widget.setLayout(layout)
@@ -63,6 +89,7 @@ class InfoWidget():
 
 class StartButton():
   def __init__(self):
+    self.in_flight = False
     self.start_button = QPushButton('Launch vessel!')
     self.start_button.setStyleSheet("background-color : green")
     self.start_button.clicked.connect(self.on_click)
@@ -71,7 +98,10 @@ class StartButton():
     return self.start_button
 
   def on_click(self):
-    print("clicked")
+    if (self.in_flight):
+      sys.exit()
+
+    self.in_flight = True
     self.start_button.setText("Launched!")
     self.start_button.setStyleSheet("background-color : red")
 
@@ -101,6 +131,14 @@ class MainWindow(QMainWindow):
 
     return scroll
 
+  def update_state(self):
+    if (not self.start_button.in_flight):
+      return
+
+    self.trajectory_plot.add_predict_point((Info.get_r_x(), Info.get_r_y()))
+
+    print("hey")
+
   def __init__(self):
     self.app = QApplication(sys.argv)
     super(MainWindow, self).__init__()
@@ -129,10 +167,14 @@ class MainWindow(QMainWindow):
     box.setLayout(layout)
 
     self.setCentralWidget(box)
-  
+    
+    self.timer = QTimer()
+    self.timer.setInterval(1000)
+    self.timer.timeout.connect(self.update_state)
+    self.timer.start()
+
   def start_application(self):
     self.show()
-
     sys.exit(self.app.exec())
 
 
