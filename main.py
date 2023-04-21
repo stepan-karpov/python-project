a1f5bdc4a61f13f5fe8a86216f8c09b212487404
StepanPC <stepan.karpov@list.ru>
2023-04-22 00:44:18 +0300

    main updated

--- main.py
+++ main.py
@@ -6,15 +6,3 @@ import time
 if __name__ == "__main__":
   window = MainWindow()
   window.start_application()
-
-  # Info.vessel.control.throttle=1
-  # time.sleep(1)
-  # print("Launch!")
-
-  # Info.vessel.control.activate_next_stage()
-
-  # while (True):
-  #   print(Info.get_vertical_speed_coordinates())
-  #   print(Info.get_horizontal_speed_coordinates())
-  #   print("=================")
-  #   time.sleep(0.5)
