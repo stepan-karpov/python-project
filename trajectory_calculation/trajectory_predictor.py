760ad1012e2da07c23c885a97d5a7002e7de7675
StepanPC <stepan.karpov@list.ru>
2023-04-22 00:00:01 +0300

    trajectory predictor updated

--- trajectory_calculation/trajectory_predictor.py
+++ trajectory_calculation/trajectory_predictor.py
@@ -13,12 +13,16 @@ class Trajectory:
   and the vessel stops it's engine at the h_end, waiting for the apoapsis to reach
 
   """
+  # trajectory by itself
   p1 = 0.8
   h_end = 40000
   h_ap = 70000
   l = 30000
   l_end = int(math.pow(h_end / h_ap, 1 / p1) * l)
   
+  # speed of the vessel configurations
+  # p2 = 
+
   @staticmethod
   def calculate_trajectory() -> None:
     """
@@ -68,16 +72,54 @@ class Trajectory:
     return 1.1
 
   @staticmethod
-  def get_ttw_r_x() -> (list, list):
+  def get_ttw_r_y() -> (list, list):
     """
-    function returns a ttw(r_x) as a pair of lists,
-    where the first list is r_x, the second is ttw
+    function returns a ttw(r_y) as a pair of lists,
+    where the first list is r_y, the second is ttw
     you can set splitting value (H_PLOT_STEP) in constants
     """
     Trajectory.check()
     ttw = []
-    r_x = []
+    r_y = []
     for i in range(0, Trajectory.l_end, H_PLOT_STEP):
       ttw.append(Trajectory.ttw(i))
-      r_x.append(i)
-    return (r_x, ttw)
+      r_y.append(i)
+    return (r_y, ttw)
+
+  @staticmethod
+  def v_x(r_x):
+    return 300
+
+  @staticmethod
+  def get_v_x_r_y() -> (list, list):
+    """
+    function returns a v_x(r_y) as a pair of lists,
+    where the first list is r_y, the second is v_x
+    you can set splitting value (H_PLOT_STEP) in constants
+    """
+    Trajectory.check()
+    v_x = []
+    r_y = []
+    for i in range(0, Trajectory.l_end, H_PLOT_STEP):
+      v_x.append(Trajectory.v_x(i))
+      r_y.append(i)
+    return (r_y, v_x)
+
+  @staticmethod
+  def v_y(r_x):
+    return 300
+
+  @staticmethod
+  def get_v_y_r_y() -> (list, list):
+    """
+    function returns a v_y(r_y) as a pair of lists,
+    where the first list is r_y, the second is v_y
+    you can set splitting value (H_PLOT_STEP) in constants
+    """
+    Trajectory.check()
+    v_y = []
+    r_y = []
+    for i in range(0, Trajectory.l_end, H_PLOT_STEP):
+      v_y.append(Trajectory.v_y(i))
+      r_y.append(i)
+    return (r_y, v_y)
