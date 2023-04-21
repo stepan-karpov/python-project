28d0dff80e4a30496f81689a4a26b21056d35bc0
StepanPC <stepan.karpov@list.ru>
2023-04-21 15:03:08 +0300

    trajectory calculations updated

--- trajectory_calculation/trajectory_predictor.py
+++ trajectory_calculation/trajectory_predictor.py
@@ -3,38 +3,81 @@ from trajectory_calculation.trajectory_constants import *
 
 class Trajectory:
   is_calculated = False
-  # trajectory configuration
-  p1 = 0.325
+  """
+  trajectory configuration
+  all of the following calculation based on the fact that we're
+  approximate rocket's trajectory by formula
+
+  r_y(r_x) = (r_x/l)^p1 * l
+
+  and the vessel stops it's engine at the h_end, waiting for the apoapsis to reach
+
+  """
+  p1 = 0.8
   h_end = 40000
   h_ap = 70000
-  L = 30000
-  h_end = math.pow(h_end / h_ap, 1 / p1) * L
+  l = 30000
+  l_end = int(math.pow(h_end / h_ap, 1 / p1) * l)
   
-  # velocity configuration
-  # v_end = 600
+  @staticmethod
+  def calculate_trajectory() -> None:
+    """
+    main calculations should be here
+    """
+    Trajectory.is_calculated = True
 
-  @classmethod
-  def calculate_trajectory(cls):
-    cls.is_calculated = True
-
-  @classmethod
-  def r_y(cls, r_x):
-    cls.check()
-    return math.pow(r_x / cls.L, cls.p1) * cls.h_ap
+  @staticmethod
+  def r_y(r_x: float) -> float:
+    """
+    function returns r_y(r_x)
+    """
+    Trajectory.check()
+    return math.pow(r_x / Trajectory.l, Trajectory.p1) * Trajectory.h_ap
   
-  @classmethod
-  def check(cls):
-    if (not cls.is_calculated):
-      cls.calculate_trajectory()
-    
-  @classmethod
-  def get_r_y_r_x(cls):
-    cls.check()
+  @staticmethod
+  def check() -> None:
+    """
+    help-function to determine whether the calculations were done or not
+    """
+    if (not Trajectory.is_calculated):
+      Trajectory.calculate_trajectory()
+
+  @staticmethod
+  def get_r_y_r_x() -> (list, list):
+    """
+    function returns a whole trajectory as a pair of lists,
+    where the first list is r_x, the second is r_y
+    you can set splitting value (H_PLOT_STEP) in constants
+    """
+    Trajectory.check()
     r_y = []
     r_x = []
-    for i in range(0, cls.L, H_PLOT_STEP):
-      r_y.append(cls.r_y(i))
+    for i in range(0, Trajectory.l_end, H_PLOT_STEP):
+      r_y.append(Trajectory.r_y(i))
       r_x.append(i)
-    return r_x, r_y
+    return (r_x, r_y)
 
+  @staticmethod
+  def ttw(r_x: float) -> float:
+    """
+    function returns ttw(r_x)
 
+    p.s. r_x in unused yet
+    """
+    Trajectory.check()
+    return 1.1
+
+  @staticmethod
+  def get_ttw_r_x() -> (list, list):
+    """
+    function returns a ttw(r_x) as a pair of lists,
+    where the first list is r_x, the second is ttw
+    you can set splitting value (H_PLOT_STEP) in constants
+    """
+    Trajectory.check()
+    ttw = []
+    r_x = []
+    for i in range(0, Trajectory.l_end, H_PLOT_STEP):
+      ttw.append(Trajectory.ttw(i))
+      r_x.append(i)
+    return (r_x, ttw)
