9e608517874f59eccd8a18f1fb4548d1ffcaf409
StepanPC <stepan.karpov@list.ru>
2023-04-22 00:31:48 +0300

    trajectory_constants modified

--- interface/trajectory_calculation_constants.py
+++ interface/trajectory_calculation_constants.py
@@ -1,15 +1,15 @@
 from flight_info.info import *
 
 INIT_FLIGH_DATA = {
-  "height": (-1, Info.get_height),
+  "height, m": (-1, Info.get_height),
   "ttw": (-1, Info.get_ttw),
-  "speed": (-1, Info.get_speed),
-  "thrust": (-1, Info.get_thrust),
-  "g": (-1, Info.get_g),
-  "pressure": (-1, Info.get_pressure)
+  "speed, m/s": (-1, Info.get_speed),
+  "thrust, N": (-1, Info.get_thrust),
+  "g, m/s^2": (-1, Info.get_g),
+  "pressure, Pa": (-1, Info.get_pressure)
 }
 
 INIT_VESSEL_DATA = {
-  "fuel on current stage": (-1, Info.get_fuel),
-  "mass of the vessel": (-1, Info.get_mass),
+  "fuel on current stage, kg": (-1, Info.get_fuel),
+  "mass of the vessel, kg": (-1, Info.get_mass),
 }
